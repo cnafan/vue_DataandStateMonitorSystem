81ece904ca73a025f44fe466f5d1a1fd1a4e6ba8
sikuquanshu123 <sikuquanshu123@hotmail.com>
2020-09-17 16:55:21 +0800

    feat:add charts

--- src/components/Splansh.vue
+++ src/components/Splansh.vue
@@ -1,15 +1,69 @@
 <template>
-  <div>
-    <el-carousel :interval="4000" type="card" height="200px">
-      <el-carousel-item v-for="item in 6" :key="item">
-      </el-carousel-item>
-    </el-carousel>
+  <!--    <el-carousel :interval="4000" type="card" height="200px">-->
+  <!--      <el-carousel-item v-for="item in 6" :key="item">-->
+  <!--      </el-carousel-item>-->
+  <!--    </el-carousel>-->
+  <div class="hello">
+    <v-chart :options="pie"></v-chart>
+
   </div>
 </template>
 
 <script>
+import ECharts from 'vue-echarts'
 export default {
-  name: 'Splansh'
+  components: {
+    chart: ECharts
+  },
+  name: 'Splansh',
+  data () {
+    return {
+      pie: {
+        title: {
+          text: '',
+          subtext: '',
+          x: 'center'
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+          orient: 'vertical',
+          left: 'left',
+          data: [
+            '系统管理控制软件',
+            'GNSS时差数据综合处理软件',
+            '卫星综合管理软件',
+            '原子钟信号监测与自主切换软件',
+            'VLBI站控软件'
+          ]
+        },
+        series: [
+          {
+            name: '来源',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '60%'],
+            data: [
+              {value: 335, name: '系统管理控制软件'},
+              {value: 310, name: 'GNSS时差数据综合处理软件'},
+              {value: 234, name: '卫星综合管理软件'},
+              {value: 135, name: '原子钟信号监测与自主切换软件'},
+              {value: 154, name: 'VLBI站控软件'}
+            ],
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            }
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
 
@@ -30,4 +84,5 @@ export default {
 .el-carousel__item:nth-child(2n+1) {
   background-color: #d3dce6;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -6,7 +6,10 @@ import router from './router'
 import ElementUI from 'element-ui'
 import store from './vuex/store'
 import 'element-ui/lib/theme-chalk/index.css'
+import ECharts from 'vue-echarts'
+import 'echarts-gl'
 
+Vue.component('v-chart', ECharts)
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 /* eslint-disable no-new */
